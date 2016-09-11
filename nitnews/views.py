bd80b107d806b349b261e55b4913d9575eca8ca6
Shariq farooq <shariqfarooq00@gmail.com>
2016-09-11 07:11:00 +0000

    Changed HttpResponse to JsonResponse

--- nitnews/views.py
+++ nitnews/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from . import scrape
 
 # Create your views here.
 def get_news(response):
-    return HttpResponse(scrape.get_news_json(),content_type='application/json')
+    return JsonResponse(scrape.get_news_json(),content_type='application/json')
