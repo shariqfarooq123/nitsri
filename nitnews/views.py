afa2612cb616846d5a85c8024d4cb699cb2d36d6
Shariq farooq <shariqfarooq00@gmail.com>
2016-09-11 13:48:13 +0000

    debugging

--- nitnews/views.py
+++ nitnews/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
+import json 
 from . import scrape
 
 # Create your views here.
 def get_news(response):
-    return JsonResponse(scrape.get_news_json(),content_type='application/json')
+    return JsonResponse(json.dumps('{"hey":"hello"}'),safe=False)
+    #return JsonResponse(scrape.get_news_json(),content_type='application/json')
