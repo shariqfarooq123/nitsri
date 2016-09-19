38eaa15a0e2da4a265857e29509f3aadb38f0cd0
Shariq farooq <shariqfarooq00@gmail.com>
2016-09-19 17:35:45 +0000

    urlpatterns change

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls.defaults import url,include
-from django.contrib.admin import admin
+#from django.contrib.admin import admin
 
 
 urlpatterns = url[
     #url(r'^admin/', 'admin.site.urls'),
-    url(r'^nitnews/','nitnews.urls')
+    url(r'^nitnews/',include('nitnews.urls'))
 ]
